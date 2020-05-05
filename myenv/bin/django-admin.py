052a13ed76c3b79d4d3ceffd97d6f05f08a1de19
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-05-05 12:41:01 -0700

    project created

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/carolynnickell/fundamentals/django-mini-project/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
