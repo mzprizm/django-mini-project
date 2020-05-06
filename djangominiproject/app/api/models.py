e7f0996600a739c0eb3789b7e691ccbfb1c24016
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-05-06 09:53:04 -0700

    added authentication, gitignore

--- djangominiproject/app/api/models.py
+++ djangominiproject/app/api/models.py
@@ -12,17 +12,31 @@ class Ep(models.Model):
     def __str__(self):
         return self.name
 
-# # class Recipe(models.Model):
-# class Song(models.Model):
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-#     category = models.ForeignKey(Category, related_name='recipes',
-#                                  on_delete=models.CASCADE)
-#     name = models.CharField(max_length=100)
+# class Category(models.Model):
+#     name = models.CharField(max_length=250)
+#     # owner = models.ForeignKey(User, on_delete=models.CASCADE)
 #     description = models.TextField(blank=True)
-#     ingredients = models.TextField()
-#     directions = models.TextField()
+#     category = models.CharField(max_length=200)
 #     created_at = models.DateTimeField(auto_now_add=True)
 #     updated_at = models.DateTimeField(auto_now=True)
 #     is_public = models.BooleanField(default=False)
 #     def __str__(self):
 #         return self.name
+
+# # class Recipe(models.Model):
+    class Song(models.Model):
+        # owner = models.ForeignKey(User, on_delete=models.CASCADE)
+        # the owner is user logging in
+        #related_name is to reference in serializers, otherwise
+        # django gives its own name. Cascading models deletes all within.
+        # category = models.ForeignKey(Category, related_name='songs',
+        #                              on_delete=models.CASCADE)
+        name = models.CharField(max_length=100)
+        description = models.TextField(blank=True)
+        ingredients = models.TextField()
+        directions = models.TextField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+        is_public = models.BooleanField(default=False)
+        def __str__(self):
+            return self.name
